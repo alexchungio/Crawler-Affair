ba9b5a87334e4b066cb567c5846ed822901a4a8c
alexchung <yonganzhong@outlook.com>
2020-06-30 18:54:36 +0800

    update  xinhuamiddleware

--- CrawlerAffair/middlewares.py
+++ CrawlerAffair/middlewares.py
@@ -5,7 +5,15 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import os
+import time
+import scrapy
 from scrapy import signals
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import ElementNotInteractableException
+
+from CrawlerAffair.spiders.xinhua import XinhuaPoliticsSpider
 
 
 class CrawleraffairSpiderMiddleware:
@@ -88,6 +96,81 @@ class CrawleraffairDownloaderMiddleware:
         # - return a Request object
         # - or raise IgnoreRequest
         return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+class XinhuaMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+    def __init__(self, delay_time):
+        self.delay_time = float(delay_time)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        delay_time = crawler.settings.get('DELAY_TIME')
+        s = cls(delay_time=delay_time)
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+
+        return s
+
+    def process_request(self, request, spider):
+
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+       return None
+
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        # chrome_options = Options()
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument('--no-sandbox')
+
+        if request.url in ["http://www.news.cn/politics/index.htm"]:
+            spider.browser.get(url=request.url)
+            load_more = spider.browser.find_elements_by_xpath('//*[@class="moreBtn"]')
+            if len(load_more) != 0:
+                while True:
+                    try:
+                        load_more = spider.browser.find_element_by_xpath('//*[@class="moreBtn"]')
+                        load_more.click()
+                        time.sleep(self.delay_time)
+                    except ElementNotInteractableException:
+                        break
+            # return selenium response
+            html = spider.browser.page_source
+            return scrapy.http.HtmlResponse(url=spider.browser.current_url, body=html.encode(), encoding="utf8", request=request)  # 参数url指当前浏览器访问的url(通过current_url方法获取), 在这里参数url也可以用request.url
+
+        else:
+            # return raw response
+            return response
+
 
     def process_exception(self, request, exception, spider):
         # Called when a download handler or a process_request()
