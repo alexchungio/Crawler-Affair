ba5b4d34606eb48afc83163810ac139e0bbdf294
alexchung <yonganzhong@outlook.com>
2020-07-07 16:29:41 +0800

    add cctv shipping spider

--- CrawlerAffair/spiders/cctv.py
+++ CrawlerAffair/spiders/cctv.py
@@ -22,6 +22,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from CrawlerAffair.utils import process_title, process_time, process_content, process_label
 from CrawlerAffair.items import CrawlerAffairItem
+from CrawlerAffair.utils import scroll
 
 
 # 无头浏览器设置
@@ -57,6 +58,7 @@ class CCTVNewsSpider(scrapy.Spider):
     def parse(self, response):
         sel = Selector(response)
 
+
         custom_menu = ["国内", "国际", "社会", "法治",  "文娱", "科技", "生活", "人物", "军事"]
         # menu_list = sel.xpath('//div[@class="nav"]/div[@class="wrap"]/a')
         menu_list = sel.xpath('//div[@class="nav_list"]/div[@class="left"]/div/span/a')
@@ -75,6 +77,7 @@ class CCTVNewsSpider(scrapy.Spider):
         news_list = sel.xpath('//*[@id="newslist"]/li/div[@class="text_con"]/h3/a/@href').extract()
         for news_url in news_list:
             # label = sel.xpath('//div[@class="v1000 clearfix bc"]/div[@class="fl w650"]/h1[@class="title]/span/text()')
+
             yield scrapy.Request(url=news_url, meta=None, callback=self.parse_detail)
 
     def parse_detail(self, response):
@@ -85,9 +88,9 @@ class CCTVNewsSpider(scrapy.Spider):
         spider_time = str(int(time.time()))
         # '/html/body/div[2]/div[3]/div/div[1]'
         publish_time = sel.xpath('//div[@class="title_area"]/div[@class="info1"]/text()').extract_first()
-        title = sel.xpath('//div[@class="title_area"]//text()').extract()
+        title = sel.xpath('//div[@class="title_area"]/h1/text()').extract()
         contents = sel.xpath('//div[@class="content_area"]/p/text()').extract()
-        labels = []
+        labels = sel.xpath('//ul[@id="searchkeywords"]/li/a/text()').extract()
 
         news_item["spider_time"] = spider_time
         news_item["publish_time"] = process_time(publish_time)
@@ -98,7 +101,116 @@ class CCTVNewsSpider(scrapy.Spider):
 
         return news_item
 
+class CCTVShipingSpider(scrapy.Spider):
+    "央视网观点"
+    name = "cctv_shiping_spider"
+    urls = []
+    allowed_domains = ["cctv.com"]
+
+    browser = webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
+
+    def start_requests(self):
+        # 'http://www.news.cn/local/wgzg.htm'
+        urls = ["https://jingji.cctv.com/shiping/index.shtml"]
+        for url in urls:
+            yield scrapy.Request(url=url, meta=None, callback=self.parse)
+
+    # 整个爬虫结束后关闭浏览器
+    def close(self, spider):
+        self.browser.quit()
+
+    # parse web html
+    def parse(self, response):
+        sel = Selector(response)
+        # '[contains(text(), ">")]'
+        self.browser.get(response.url)
+        # 'data-spm-anchor-id="C87458.PehgQlaw4J7u.EbPec9NH7wI8.1225"'
+        for i in range(30):
+            yield scrapy.Request(url=sel.response.url, meta=None, callback=self.parse_sub_page, dont_filter=True)
+            scroll(self.browser)
+            next_page = self.browser.find_element_by_xpath('//span[@class="tpb_right"]/a[contains(text(), ">")]')
+            next_page.click()
+
+    def parse_sub_page(self, response):
+
+        sel = Selector(response)
+        news_list = sel.xpath('//div[@class="img_title_list"]/div/h2/a/@href').extract()
+        for news_url in news_list:
+            # label = sel.xpath('//div[@class="v1000 clearfix bc"]/div[@class="fl w650"]/h1[@class="title]/span/text()')
+
+            yield scrapy.Request(url=news_url, meta=None, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+
+        sel = Selector(response)
+
+        news_item = CrawlerAffairItem()
+        spider_time = str(int(time.time()))
+        # '/html/body/div[2]/div[3]/div/div[1]'
+        publish_time = sel.xpath('//div[@class="function"]/span[@class="info"]/i/text()').extract()
+        title = sel.xpath('//div[@class="cnt_bd"]/h1/text()').extract()
+        contents = sel.xpath('//div[@class="cnt_bd"]/p/text()').extract()
+        labels = []
+
+
+        news_item["spider_time"] = spider_time
+        news_item["publish_time"] = process_time(publish_time)
+        news_item["title"] = process_title(title)
+        news_item["label"] = process_label(labels)
+        news_item["content"] = process_content(contents)
+        news_item['url'] = sel.response.url.strip()
+
+        return news_item
 
 # class XinhuaPoliticsSpider(XinhuaCommonSpider):
 #     name = "xinhua_politics_spider"
 #     urls = ['http://www.news.cn/politics/index.htm', 'http://www.news.cn/politics/xgc.htm']
+
+class CCTVCaijingSpider(scrapy.Spider):
+    "央视网观点"
+    name = "cctv_caijing_spider"
+    urls = ["https://jingji.cctv.com/caijing/index.shtml"]
+    allowed_domains = ["cctv.com"]
+
+    browser = webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
+
+    def start_requests(self):
+
+        for url in self.urls:
+            yield scrapy.Request(url=url, meta=None, callback=self.parse)
+
+    # 整个爬虫结束后关闭浏览器
+    def close(self, spider):
+        self.browser.quit()
+
+    # parse web html
+    def parse(self, response):
+        # '[contains(text(), ">")]'
+        sel = Selector(response)
+        news_list = sel.xpath('//*[@id="leftContent"]/div/h3/span/a/@href').extract()
+        for news_url in news_list:
+            # label = sel.xpath('//div[@class="v1000 clearfix bc"]/div[@class="fl w650"]/h1[@class="title]/span/text()')
+
+            yield scrapy.Request(url=news_url, meta=None, callback=self.parse_detail)
+
+
+    def parse_detail(self, response):
+
+        sel = Selector(response)
+
+        news_item = CrawlerAffairItem()
+        spider_time = str(int(time.time()))
+        # '/html/body/div[2]/div[3]/div/div[1]'
+        publish_time = sel.xpath('//div[@class="function"]/span[@class="info"]/i/text()').extract()
+        title = sel.xpath('//div[@class="cnt_bd"]/h1/text()').extract()
+        contents = sel.xpath('//div[@class="cnt_bd"]/p/text()').extract()
+        labels = []
+
+        news_item["spider_time"] = spider_time
+        news_item["publish_time"] = process_time(publish_time)
+        news_item["title"] = process_title(title)
+        news_item["label"] = process_label(labels)
+        news_item["content"] = process_content(contents)
+        news_item['url'] = sel.response.url.strip()
+
+        return news_item
