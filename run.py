979798d3fc32d9d75403ca132022e192bb818eed
alexchung <yonganzhong@outlook.com>
2020-07-10 08:55:35 +0800

    update run module

--- run.py
+++ run.py
@@ -20,6 +20,9 @@ from scrapy.crawler import CrawlerRunner
 from twisted.internet import reactor
 from scrapy.utils.log import configure_logging
 
+from CrawlerAffair.spiders.china import ChinaAffairSpider, ChinaNewsSpider
+from CrawlerAffair.spiders.fujian import FujianInfoSpider
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
@@ -31,5 +34,35 @@ if __name__ == "__main__":
 
     # method 2 CrawlerProcess（recommend）
     process = CrawlerProcess(get_project_settings())
-    process.crawl("china_spide", domain={"china.com.cn"})
+
+    # process.crawl(ChinaNewsSpider, domain={"china.com.cn"})
+    # process.crawl(ChinaAffairSpider, domain={"china.com.cn"})
+
+    # process.crawl("china_news_spider", domain={"china.com.cn"})
+    # process.crawl("china_affair_spider", domain={"china.com.cn"})
+    # process.crawl("china_opinion_spider", domain={"china.com.cn"})
+    # process.crawl("china_theory_spider", domain={"china.com.cn"})
+    #
+    # process.crawl("renmin_politics_spider", domain={"china.com.cn"})
+
+    # process.crawl("xinhua_politics_spider", domain={"news.cn"})
+    # process.crawl("xinhua_local_spider", domain={"news.cn"})
+    # process.crawl("xinhua_legal_spider", domain={"news.cn"})
+    # process.crawl("xinhua_renshi_spider", domain={"news.cn"})
+    # process.crawl("xinhua_info_spider", domain={"news.cn"})
+    # process.crawl("xinhua_siklroad_spider", domain={"news.cn"})
+
+    # process.crawl("cctv_news_spider", domain={"cctv.com"})
+    # process.crawl("cctv_shiping_spider", domain={"cctv.com"})
+    # process.crawl("cctv_caijing_spider", domain={"cctv.com"})
+
+    # process.crawl("chinadaily_china_spider", domain={"chinadaily.com.cn"})
+    # process.crawl("chinadaily_world_spider", domain={"chinadaily.com.cn"})
+
+    # process.crawl("thepaper_select_spider", domain={"thepaper.cn"})
+    # process.crawl("thepaper_shishi_spider", domain={"thepaper.cn"})
+
+
+    process.crawl(FujianInfoSpider, domain={"fujian.gov.cn"})
     process.start()
+
